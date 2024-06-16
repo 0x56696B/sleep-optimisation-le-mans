ade109613f77130705031d1d63a9abc509804c70
Viktor Stamoff <transtrike@gmail.com>
2024-06-16 10:40:38 +0300

    added fetching the team in analyze service

--- apps/back-end/src/services/analyze.service.ts
+++ apps/back-end/src/services/analyze.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Team } from 'src/models/Team';
+import AppDataSource from 'typeorm.config';
 
 @Injectable()
 export class AnalyzeService {
   public async analyzeTeam(teamId: number) {
+    const teamRepo = AppDataSource.getRepository(Team);
+
+    const team = await teamRepo.findOne({
+      where: { id: teamId },
+      relations: ['people', 'people.activityLevel', 'people.shifts'],
+    });
+
+    // Perform additional analysis if needed
+    console.log({ team });
+
     //TODO: Fetch team from database with all drivers/managers/strategists
     //TODO: analyze
     //
